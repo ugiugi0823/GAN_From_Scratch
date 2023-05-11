d4f8b1c9abffc3a40fe1e7db726650b44efa9018
Hyunwook Jo <acerghjk@gmail.com>
2023-05-11 15:30:48 +0900

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# Utils.py
 import os
+import torch
 import torchvision
 
 import numpy as np
@@ -13,12 +9,14 @@ from PIL import Image
 from torchvision.utils import make_grid
 
 def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    
     '''
     Function for visualizing images: Given a tensor of images, number of images, and
     size per image, plots and prints the images in a uniform grid.
     '''
     image_unflat = image_tensor.detach().cpu().view(-1, *size)
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.figure()
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
     plt.show()
     
